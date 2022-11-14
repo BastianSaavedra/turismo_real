bc30a63119e8991f48944dd4225b8ef4f8b5591a
SebasZP <sebax09zp@outlook.com>
2022-11-14 15:23:24 -0300

    Cancelacion y agregacion de servicios extra

--- webTurismoReal/home/urls.py
+++ webTurismoReal/home/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('user/reserva-dpto/reserva', views.home_reserva, name="home_reserva"),
     path('user/mis-reservas', views.home_reservas_usuario, name="home_reservas_usuario"),
     path('user/mis-reservas/cancelar-reserva/<id>/', views.cancelar_reserva, name="home_cancelar_reserva"),
+    path('user/mis-reservas/reserva=<int:id>', views.detalle_reserva, name="detalle_reserva"),
+    path('user/mis-reservas/reserva/edit/<id>/', views.agregar_servicios, name="agregar_servicios"),
+    path('user/mis-reservas/cancelar-tour/<id>/', views.cancelar_tour, name="home_cancelar_tour"),
+    path('user/mis-reservas/cancelar-transporte/<id>/', views.cancelar_transporte, name="home_cancelar_transporte"),
     # path('user/arriendo-dpto/arriendo', arriendo_confirmacion, name="arriendo_dpto"),
-
-    
 ]
